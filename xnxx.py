23af2593b6c2e96910aa558813e6b425ec39152c
Henrycko <49187820+Henrycko@users.noreply.github.com>
2019-04-24 11:58:26 +0700

    Update xnxx.py

--- xnxx.py
+++ xnxx.py
@@ -8,7 +8,7 @@ import subprocess as sp
 subprocess.call("clear", shell=True)
 
 banner = """
-╔╗╴╴╴╔══════╗ \033[1;34mCalling SPAM\033[1;36m UNLIMITED\033[0m
+╔╗╴╴╴╔══════╗ \033[1;34mCall SPAM\033[1;36m UNLIMITED\033[0m
 ║║╴╴╴║╴╔════╝
 ║╚═══╝╴╚════╗ Penyusun  : \033[1;32@thetermuxchoice01\033[0m
 ╚════╗╴╔═══╗║ Pendukung : Dzakira Alzena Daiva
