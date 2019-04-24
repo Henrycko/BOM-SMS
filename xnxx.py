833b72b6617218e47d74a80bd24eb915a84483e0
Henrycko <49187820+Henrycko@users.noreply.github.com>
2019-04-24 11:36:53 +0700

    Update xnxx.py

--- xnxx.py
+++ xnxx.py
@@ -10,7 +10,7 @@ subprocess.call("clear", shell=True)
 banner = """
 ╔╗╴╴╴╔══════╗ \033[1;34mCalling SPAM\033[1;36m UNLIMITED\033[0m
 ║║╴╴╴║╴╔════╝
-║╚═══╝╴╚════╗ Author    : \033[1;32@thetermuxchoice01\033[0m
+║╚═══╝╴╚════╗ Penyusun  : \033[1;32@thetermuxchoice01\033[0m
 ╚════╗╴╔═══╗║ Pendukung : Dzakira Alzena Daiva
 ╔════╝╴║╴╴╴║║ 
 ╚══════╝╴╴╴╚╝
