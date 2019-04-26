7c295dd4cfa57f13f44a2b710ea85f14729f5c5f
Henrycko <49187820+Henrycko@users.noreply.github.com>
2019-04-26 08:19:20 +0700

    Update and rename XNXX-OTP.py to XNXX-OTP.php

---
+++ XNXX-OTP.php
@@ -0,0 +1,27 @@
+<?php
+system("apt-get install toilet -y");
+system("toilet -f future FakeCall --gay");
+echo "
+[====================]
+ SPAM CALL UNLIMITED
+[====================]
+";
+function api($nomor,$jumlah){
+$url = "https://0x.nakocoders.org/rest-api/lain-nya/api.php?nomor=$nomor";
+$loop = 0;
+while($loop < $jumlah){
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+$hasil = curl_exec($ch);
+echo $hasil;
+sleep(5);
+$loop++;
+}
+}
+echo "Masukan Nomor : ";
+$nomor = trim(fgets(STDIN));
+echo "Masukan Jumlah : ";
+$jumlah = trim(fgets(STDIN));
+api($nomor,$jumlah);
+?>
